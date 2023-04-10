2302fd006342da64f82021b496b8dbf65cb7c086
H.F <110806387+HF1252@users.noreply.github.com>
2023-04-10 22:37:47 +0900

    Initial commit
    Training

---
+++ ShopSite-Desig-2023ver/ShopSite.js
@@ -0,0 +1,32 @@
+//Menu
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+//cart item
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+//search form
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
